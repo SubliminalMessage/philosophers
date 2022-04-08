c8a0156aba3b9acfe2b81c33bd77cf034ed927e8
DanielGonzalezCarretero <glezdani125@gmail.com>
2022-04-08 17:03:03 +0200

    yuh

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -2,11 +2,19 @@
 # define PHILO_H
 
 # include <unistd.h>
-# include <stdio.h>
+# include <stdio.h> /* <?? */
 # include <string.h>
 # include <stdlib.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+// Util Functions
+int		ft_atoi(const char *str);
+char	**ft_split(const char *s, char c);
+int		ft_isdigit(int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
 
 typedef struct s_config {
     int n_philo;
--- philo/src/main.c
+++ philo/src/main.c
@@ -7,16 +7,42 @@ void    clean_exit(char *msg)
 	exit(0);
 }
 
+t_config    *parse_configuration(int argc, char **argv)
+{
+    t_config *config;
+
+    config = malloc(sizeof(t_config));
+    config->n_philo = ft_atoi(argv[1]);
+    config->time_die = ft_atoi(argv[2]);
+    config->time_eat = ft_atoi(argv[3]);
+    config->time_sleep = ft_atoi(argv[4]);
+    if (argc == 6)
+        config->total_loops = ft_atoi(argv[5]);
+    else
+        config->total_loops = -1;
+
+    /* Print result */
+    printf("Config file:\n");
+    printf("Nº Philos:    \t%d\n", config->n_philo);
+    printf("Time to die:  \t%dms\n", config->time_die);
+    printf("Time to eat:  \t%dms\n", config->time_eat);
+    printf("Time to sleep:\t%dms\n", config->time_sleep);
+    printf("Total loops:  \t%d\n", config->total_loops);
+
+    return (config);
+}
+
 /*
 number_of_philosophers time_to_die
 time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
 */
 int main(int argc, char **argv)
 {
+    t_config *config;
     // Parse Args
     if (argc != 5 && argc != 6)
         clean_exit("Not enough arguments or too many of them!\n");
-
+    config = parse_configuration(argc, argv);
     // Execute Philos
     // Clean exit i guess
 
