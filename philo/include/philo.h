bff7554e236329f78613519e74214c23cc0a4411
dangonza <dutchatewarra@gmail.com>
2022-04-15 01:22:38 +0200

    Forks are not correctly given to philos. Thus, infinite loops happend

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -7,6 +7,7 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <pthread.h>
+# include <stddef.h>
 
 // Util Functions
 int		ft_atoi(const char *str);
@@ -15,15 +16,9 @@ int		ft_isdigit(int c);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
+int		ft_strdigit(char *s);
 
-typedef struct s_config {
-    int n_philo;
-    int time_die;
-    int time_eat;
-    int time_sleep;
-    int total_loops;
-    int *forks; // Change to mutex
-} t_config;
+typedef pthread_mutex_t t_mutex;
 
 typedef struct s_philo {
     int index;
@@ -31,6 +26,22 @@ typedef struct s_philo {
     int time_eat;
     int time_sleep;
     int total_loops;
-}   t_philo;
+    int current_loops;
+    int fork_index; // 0 or 1, to then % 2 for sync-ing
+    t_mutex *forks[2];
+    t_mutex **print_mutex;
+} t_philo;
+
+typedef struct s_config {
+    int n_philo;
+    int time_die;
+    int time_eat;
+    int time_sleep;
+    int total_loops;
+    pthread_t *threads; // free()
+    t_philo **philos; // free()
+    t_mutex *forks; // free() / destroy/()
+    t_mutex *print_mutex; // free() / destroy()
+} t_config;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -1,50 +1,138 @@
 #include <philo.h>
 
-void    clean_exit(char *msg)
+void    clean_exit(char *msg, t_config *config)
 {
 	if (msg != NULL)
 		printf("%s\n", msg);
 	exit(0);
+
+    (void) config;
 }
 
 t_config    *parse_configuration(int argc, char **argv)
 {
-    t_config *config;
+    t_config *cfg;
+    int i;
 
-    config = malloc(sizeof(t_config));
-    config->n_philo = ft_atoi(argv[1]);
-    config->time_die = ft_atoi(argv[2]);
-    config->time_eat = ft_atoi(argv[3]);
-    config->time_sleep = ft_atoi(argv[4]);
+    if (!ft_strdigit(argv[1]) || !ft_strdigit(argv[2]) 
+    || !ft_strdigit(argv[3]) || !ft_strdigit(argv[4]))
+        clean_exit("Arguments can only be numbers", NULL);
+    cfg = malloc(sizeof(t_config));
+    cfg->n_philo = ft_atoi(argv[1]);
+    cfg->time_die = ft_atoi(argv[2]);
+    cfg->time_eat = ft_atoi(argv[3]);
+    cfg->time_sleep = ft_atoi(argv[4]);
     if (argc == 6)
-        config->total_loops = ft_atoi(argv[5]);
+    {
+        if (!ft_strdigit(argv[5]))
+            clean_exit("Arguments can only be numbers", cfg);
+        cfg->total_loops = ft_atoi(argv[5]);
+    }
     else
-        config->total_loops = -1;
+        cfg->total_loops = -1;
+    if (cfg->n_philo <= 0)
+        clean_exit("There should be at least 1 Philosopher", cfg);
+
+    cfg->threads = malloc(sizeof(pthread_t) * cfg->n_philo);
+    cfg->philos = malloc(sizeof(t_philo *) * cfg->n_philo);
+    cfg->forks = malloc(sizeof(t_mutex) * cfg->n_philo);
+
+    if (cfg->threads == NULL || cfg->philos == NULL || cfg->forks == NULL)
+        clean_exit("Malloc failed :(", cfg);
+
+    i = 0;
+    while (i < cfg->n_philo)
+    {
+        pthread_mutex_init(&cfg->forks[i], NULL);
+        i++;
+    }
 
     /* Print result */
-    printf("Config file:\n");
-    printf("Nº Philos:    \t%d\n", config->n_philo);
-    printf("Time to die:  \t%dms\n", config->time_die);
-    printf("Time to eat:  \t%dms\n", config->time_eat);
-    printf("Time to sleep:\t%dms\n", config->time_sleep);
-    printf("Total loops:  \t%d\n", config->total_loops);
+    printf("Config args:\n");
+    printf("Nº Philos:    \t%d\n", cfg->n_philo);
+    printf("Time to die:  \t%dms\n", cfg->time_die);
+    printf("Time to eat:  \t%dms\n", cfg->time_eat);
+    printf("Time to sleep:\t%dms\n", cfg->time_sleep);
+    printf("Total loops:  \t%d\n", cfg->total_loops);
 
-    return (config);
+    return (cfg);
+}
+
+void    *run_philo(void *arg)
+{
+    t_philo *philo = (t_philo *) arg;
+    
+    printf("Nº %d running\n", philo->index);
+
+    while(1)
+    {
+        printf("Nº %d trying to grab [0]\n", philo->index);
+        pthread_mutex_lock(philo->forks[0]);
+        printf("Nº %d -> Locked [0]!\n", philo->index);
+        printf("Nº %d trying to grab [1]\n", philo->index);
+        pthread_mutex_lock(philo->forks[1]);
+        printf("Nº %d -> Locked [1]!\n", philo->index);
+        printf("Nº %d -> Waiting 1000ms...\n", philo->index);
+        usleep(100);
+        printf("Nº %d -> Dropping both forks...\n", philo->index);
+        pthread_mutex_unlock(philo->forks[0]);
+        pthread_mutex_unlock(philo->forks[1]);
+
+        printf("Nº %d -> Dropped!! Restarting in 1000ms\n", philo->index);
+        usleep(100);
+    }
+
+    return (NULL);
 }
 
 /*
 number_of_philosophers time_to_die
 time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
 */
+
+t_philo *create_philo(t_config *cfg, int index)
+{
+    t_philo *philo;
+
+    philo = malloc(sizeof(t_philo));
+    philo->index = index;
+    philo->time_die = cfg->time_die;
+    philo->time_eat = cfg->time_eat;
+    philo->time_sleep = cfg->time_sleep;
+    philo->total_loops = cfg->total_loops;
+    philo->current_loops = 0;
+    philo->fork_index = 0; // <??
+    philo->forks[1] = &cfg->forks[index - 1];
+    if (index == 1)
+        philo->forks[0] = &cfg->forks[cfg->n_philo - 1];
+    else
+        philo->forks[0] = &cfg->forks[index % cfg->n_philo];
+
+    return(philo);
+}
+
 int main(int argc, char **argv)
 {
-    t_config *config;
-    // Parse Args
-    if (argc != 5 && argc != 6)
-        clean_exit("Not enough arguments or too many of them!\n");
-    config = parse_configuration(argc, argv);
-    // Execute Philos
-    // Clean exit i guess
+    t_config    *config;
+    int         i;
 
-    (void) argv;
+    if (argc != 5 && argc != 6)
+        clean_exit("Not enough arguments or too many of them!", NULL);
+    config = parse_configuration(argc, argv);
+
+    i = 0;
+    while (i < config->n_philo)
+    {
+        config->philos[i] = create_philo(config, i + 1);
+        pthread_create(&config->threads[i], NULL, run_philo, config->philos[i]);
+        i++;
+    }
+    
+    i = 0;
+    while (i < config->n_philo)
+    {
+        pthread_join(config->threads[i], NULL);
+        i++;
+    }
+    printf("Finished!\n");
 }
